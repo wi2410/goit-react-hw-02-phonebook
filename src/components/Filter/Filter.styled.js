f8b4c6a5ea097b44dd62c74f9f395ec351c344ef
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-11-06 21:59:56 +0200

    add filter

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const FilterInput = styled.input`
+  border: 1px solid black;
+  box-shadow: 10px 10px 12px -3px rgba(0, 0, 0, 0.21);
+  margin-bottom: 16px;
+  margin-top: 8px;
+  padding: 4px;
+  outline: none;
+  &:hover,
+  :focus {
+    border: 1px solid skyblue;
+  }
+`;
