731c76f182a6341e6afcd80862c160b6067ae677
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-11-06 22:20:49 +0200

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const FilterInput = styled.input`
   border: 1px solid black;
-  box-shadow: 10px 10px 12px -3px rgba(0, 0, 0, 0.21);
+  box-shadow: 4px 4px 4px 1px rgba(0, 0, 0, 0.21);
   margin-bottom: 16px;
   margin-top: 8px;
   padding: 4px;
